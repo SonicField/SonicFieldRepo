b21c336888b244b74a32b046e304cb455cc5029d
Alexander Turner <nerdscentral@googlemail.com>
2014-12-11 09:34:20 +0000

    Add missing .sy
    New patches missing are added plus new Finalise operator (called
    CleanFix for historical reasons)

---
+++ SonicField/src/com/nerdscentral/audio/volume/SF_CleanFix.java
@@ -0,0 +1,65 @@
+/* For Copyright and License see LICENSE.txt and COPYING.txt in the root directory */
+package com.nerdscentral.audio.volume;
+
+import com.nerdscentral.audio.SFData;
+import com.nerdscentral.audio.SFSignal;
+import com.nerdscentral.audio.SFSingleTranslator;
+import com.nerdscentral.sython.Caster;
+import com.nerdscentral.sython.SFMaths;
+import com.nerdscentral.sython.SFPL_Context;
+import com.nerdscentral.sython.SFPL_Operator;
+import com.nerdscentral.sython.SFPL_RuntimeException;
+
+public class SF_CleanFix implements SFPL_Operator
+{
+    static class Translator extends SFSingleTranslator
+    {
+        final double scale;
+
+        protected Translator(SFSignal input, double scaleIn)
+        {
+            super(input);
+            scale = scaleIn;
+        }
+
+        @Override
+        public double getSample(int index)
+        {
+            double r = getInputSample(index) * scale;
+            if (SFMaths.abs(r) > 1.0)
+            {
+                // System.err.println("Err " + r + "," + scale);
+            }
+            return r;
+        }
+    }
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public Object Interpret(final Object input, final SFPL_Context context) throws SFPL_RuntimeException
+    {
+        try (SFSignal sig = Caster.makeSFSignal(input))
+        {
+            try (SFData dataIn = SFData.realise(sig); SFData dataOut = dataIn.replicateEmpty();)
+            {
+                if (dataIn == sig) dataIn.incrReference();
+
+                double max = SF_Clean.decimateFilter(dataIn, dataOut);
+                max = 1 / max;
+                Translator ret = new Translator(dataOut, max);
+                return ret;
+            }
+        }
+    }
+
+    @Override
+    public String Word()
+    {
+        return Messages.getString("SF_CleanFix.0"); //$NON-NLS-1$
+    }
+
+}
