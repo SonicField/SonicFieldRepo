977b29ec1c1c4f5a718bb8755a7e860d092c7388
Alexander Turner <nerdscentral@googlemail.com>
2016-04-01 18:31:14 +0100

    Pattern buffer

--- SonicField/scripts/2016/analogue-chorus.py
+++ SonicField/scripts/2016/analogue-chorus.py
@@ -1,9 +1,12 @@
-Sig = sf.MakeTriangle(sf.SineWave(10000,440))
+from Filters import byquad_filter
+import random
+sig = sf.SineWave(10000,440)
 env = sf.NumericShape((0,0),(10,1),(1024,0.1),(10000,0))
 sig = +sf.Multiply(env,sig)
-dhz = 8
-dly = 1000.0/float(dhz);
-mod = sf.SineWave(10000,1.0)
+dhz = 32
+dly = 1000.0/float(dhz)
+frq = 32
+mod = +sig
 sig = sf.AnalogueChorus(sig,dly,mod,0.75,1.25)
 out = [ sf.FixSize(s) for s in sig ]
 sf.WriteFile32(out,'temp/ac.wav')
