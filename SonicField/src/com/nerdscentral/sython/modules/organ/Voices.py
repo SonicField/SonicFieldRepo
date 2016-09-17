24fcce488c36dca24b8f6fef63e3e502e1cf39e4
Alexander Turner <nerdscentral@googlemail.com>
2016-09-17 22:19:16 +0100

    Reasonant Beauty

--- SonicField/src/com/nerdscentral/sython/modules/organ/Voices.py
+++ SonicField/src/com/nerdscentral/sython/modules/organ/Voices.py
@@ -884,7 +884,11 @@ def folk_flute(length,freq):
     return sf.FixSize(polish(sig,freq))
 
 def _distant_wind(length, freq, qCorrect = 1.25):
-    base = clean_noise(length,freq*4.0)
+    length += 250.0
+    base = sf.Mix(
+        clean_noise(length,freq*4.0),
+        sf.Volume(sf.SineWave(length,freq), -60)
+    )
     env = []
     # Super imposted last two postions does not matter.
     v = 0.5
@@ -900,7 +904,7 @@ def _distant_wind(length, freq, qCorrect = 1.25):
         # Constrained random walk envelope in 100 ms steps.
         t += 100
 
-    base = sf.Multiply(sf.NumericShape(env), base)
+    #base = sf.Multiply(sf.NumericShape(env), base)
     out = []
     xq = 1.8 if freq > 640 else 2.0 if freq > 256 else 2.5 if freq > 128 else 3.0
     xq *= qCorrect
@@ -916,18 +920,29 @@ def _distant_wind(length, freq, qCorrect = 1.25):
         ]
     -base
     out = sf.Mix(out)
-    return sf.ButterworthLowPass(out, freq*1.25, 6)
+    out = sf.ButterworthLowPass(out, freq*1.25, 4)
+    out = sf.ButterworthLowPass(out, freq*1.25, 4)
+    st = sf.Cut(0, length/2.0,+out)
+    ed = sf.Cut(length/2.0, length,+out)
+    st = sf.Magnitude(st)
+    ed = sf.Magnitude(ed)
+    rt = st/ed
+    ev = sf.NumericShape((0.0, 1.0), (length, rt))
+    out = sf.Multiply(ev, out)
+    return sf.FixSize(sf.Cut(250,length,out))
 
 def distant_wind(length, freq):
 
     @sf_parallel
     def doWind(lenth, hfreg):
-        out = _distant_wind(length, hfreq)
+        outF = _distant_wind(length, hfreq)
+        outR = _distant_wind(length, hfreq)
+        out  = sf.FixSize(sf.Reverse(outR), outF)
         if freq > 256:
             out = sf.Power(out, 1.25)
         else:
             out = sf.Power(out, 1.05)
-            return sf.FixSize(polish(out, hfreq))
+        return sf.FixSize(polish(out, hfreq))
 
     sigs = []
     harms = range(1, 15)
@@ -951,10 +966,7 @@ def additive_resonance(power, qCorrect, saturate, rollOff, post, length, freq):
     @sf_parallel
     def doWind(lenth, hfq):
         out = _distant_wind(length, hfq, qCorrect)
-        if freq > 256:
-            out = sf.Power(out, pow(1.25, power))
-        else:
-            out = sf.Power(out, pow(1.05, power))
+        out = sf.Power(out, power)
         if saturate:
             os = sf.Saturate(sf.NumericVolume(sf.FixSize(+out), 2.0))
             out = sf.Mix(sf.NumericVolume(out, 1.0-saturate), sf.NumericVolume(os, saturate))
@@ -985,22 +997,50 @@ def additive_resonance(power, qCorrect, saturate, rollOff, post, length, freq):
     ret = stage2(harms, sigs)
     return post(ret, length, freq) if post else ret
 
-def make_addtive_resonance(power = 1.0 , qCorrect = 1.25 , saturate = 0.0, rollOff = 4.0, post = None):
+def make_addtive_resonance(power = 1.1 , qCorrect = 1.25 , saturate = 0.0, rollOff = 4.0, post = None):
     return functools.partial(additive_resonance, power, qCorrect, saturate, rollOff, post)
 
 def oboe_filter(sig, length, freq):
-    #sig = polish(sig,freq)
-    #sig = sf.FixSize(sf.Power(sig,1.5))
-    sig = polish(sig,freq)
-    sig = sf.FixSize(sf.Power(sig,1.5))
-    sig = sf.FixSize(polish(sig, freq))
-    sig = sf.RBJPeaking(sig,freq*5,0.5,5)
-    sig = sf.RBJPeaking(sig,freq*7,1,5)
-    sig = sf.RBJNotch(sig,freq*2,1.0,1.0)
+    sig = sf.RBJPeaking(sig, freq*3, 0.2, 5)
+    sig = sf.RBJPeaking(sig, freq*5, 0.5, 4)
+    sig = sf.RBJPeaking(sig, freq*7, 1, 4)
+    sig = sf.RBJNotch(sig, freq*2, 1.0, 1.0)
+    sig = sf.Mix(+sig, sf.RBJNotch(sig, freq, 1.0, 1.0))
+    sig = sf.RBJLowPass(sig, freq*9, 1.0)
     br = freq * 9
     if br > 5000.0:
-       br = 5000.0 
-    sig = sf.ButterworthLowPass(sig, br, 2)
+       br = 5000.0
+    sig = sf.RBJLowPass(sig, br, 1.0)
+    return sf.FixSize(sf.Clean(sig))
+
+@sf_parallel
+def violin_filter(sig, length, freq):
+    sigs=[]
+    bodies = violinIRs
+    for body in bodies:
+        sigs.append(convolve(+sig,+body))
+    sig = sf.Mix(
+        sf.FixSize(sf.Clean(sf.Mix(sigs))),
+        sig
+    )
+    
+    if length > 250:
+        # TODO feels a bit crushed - more stages?
+        vibStart  = length*0.5  if length>600 else vibAbove*0.75
+        vibMiddle = length*0.75 if length>600 else vibAbove
+        vibAmount = 0.5 if length > 1000 else 0.25
+        trueLen = sf.Length(+sig)
+        l = trueLen
+        env = sf.NumericShape((0, 0), (vibStart, 0), (vibMiddle, 1), (length, 0.75), (l, 0))
+        env = sf.NumericVolume(env, vibAmount)
+        trem = sf.SineWave(l,2.0 + random.random())
+        trem = sf.Multiply(env, trem)
+        vib = +trem
+        trem = sf.DirectMix(1, sf.Pcnt50(trem))
+        sig = sf.Multiply(trem, sig)
+        vib = sf.DirectMix(1, sf.NumericVolume(vib, 0.01))
+        sig = sf.Resample(vib, sig)
+
     return sf.FixSize(sf.Clean(sig))
 
 @sf_parallel
